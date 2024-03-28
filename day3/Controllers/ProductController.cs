ff005d3a9a5a8c6229d60e386031e5fcc45f63de
Linh <khanhlinhdh.work@gmail.com>
2024-03-28 23:44:17 +0700

    first-commit

---
+++ day3/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using day3.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace day3.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        ASPEntities ctx = new ASPEntities();
+        public async Task<ActionResult> Index()
+        {
+            var products = await ctx.demoASP1.ToListAsync();
+            return View(products);
+        }
+
+        public async Task<ActionResult> Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Create(demoASP1 p)
+        {
+            ctx.demoASP1.Add(p);
+            await ctx.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ day3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(day3.Startup))]
+namespace day3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
